ab4cafdc836943ddb204037554cddc05f432e30f
Tianyi Kao (Max) <tianyikao@gmail.com>
2023-05-24 08:35:55 -0700

    message

--- Demo.java
+++ Demo.java
@@ -1,5 +1,10 @@
 public class Demo {
     public static void main(String[] args){
         System.out.println("Da!");
+        long flightNumber = 1000;
+        flightNumber = flightNumber + 1;
+        System.out.println(flightNumber);
     }
 }
+
+
