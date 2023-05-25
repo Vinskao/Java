62b88191c7d929f2ad0a56bd77d1e5bc93781874
Tianyi Kao (Max) <tianyikao@gmail.com>
2023-05-25 00:12:45 -0700

    message

--- Demo.java
+++ Demo.java
@@ -1,9 +1,21 @@
+import javax.swing.LayoutStyle;
+
 public class Demo {
     public static void main(String[] args){
         System.out.println("Da!");
+        //int
         long flightNumber = 1000;
         flightNumber = flightNumber + 1;
         System.out.println(flightNumber);
+        //boolean variables
+        int tits = 5;
+        boolean bra = tits > 3;
+        System.out.println(bra);
+        //string variables, string is a class, first letter is uppercase
+        String label = "king:";
+        String firstname = "m" ;
+        String lastname = "kao";
+        System.out.println(label + firstname + lastname);
     }
 }
 
