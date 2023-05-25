ecf8fdd83f4af43194f658c46c3558c92148a14d
Tianyi Kao (Max) <tianyikao@gmail.com>
2023-05-25 07:42:31 -0700

    message

--- Demo.java
+++ Demo.java
@@ -1,3 +1,6 @@
+import java.time.LocalDate;
+import java.util.jar.Attributes.Name;
+
 import javax.swing.LayoutStyle;
 
 public class Demo {
@@ -7,6 +10,8 @@ public class Demo {
         long flightNumber = 1000;
         flightNumber = flightNumber + 1;
         System.out.println(flightNumber);
+        //float type, double type; float is not accurate; 32 bits float; 64 bits double
+        double total = 10d;
         //boolean variables
         int tits = 5;
         boolean bra = tits > 3;
@@ -16,6 +21,8 @@ public class Demo {
         String firstname = "m" ;
         String lastname = "kao";
         System.out.println(label + firstname + lastname);
+        LocalDate invoiceLocalDate = LocalDate.now();
+        System.out.println(invoiceLocalDate);
     }
 }
 
---
+++ passenger.java
@@ -0,0 +1,16 @@
+Package org.airline.reservations;
+public class passenger {
+    
+    Private String name;
+    public Passenger(){
+        name = "unknown name"
+
+    public String getName(){
+        return Name;
+    }
+    public void setName(String newName){
+        name = newName;
+    }
+}
+
+}
